c8e0dd8c769e39e62d7416d5502b41cf805713e8
Leandro <63678585+Liuth16@users.noreply.github.com>
2024-10-20 06:29:13 -0300

    Fix identation

--- js-file.js
+++ js-file.js
@@ -8,32 +8,32 @@ const form = document.querySelector("#book-form");
 const myLibrary = [];
 
 class Book {
-    constructor (author, title, pages) {
-        this.author = author;
-        this.title = title;
-        this.pages = pages;
-        this.dataIndex = myLibrary.length;
-        this.read = false;
-    }
-};
+  constructor(author, title, pages) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.dataIndex = myLibrary.length;
+    this.read = false;
+  }
+}
 
 function addBookToLibrary(author, title, pages) {
-    const book = new Book(author, title, pages);
-    const index = book.dataIndex;
-    const keyName = `book${index}`;
-    const bookEntry = {};
-    bookEntry[keyName] = book;
-    myLibrary.push(bookEntry);
-};
+  const book = new Book(author, title, pages);
+  const index = book.dataIndex;
+  const keyName = `book${index}`;
+  const bookEntry = {};
+  bookEntry[keyName] = book;
+  myLibrary.push(bookEntry);
+}
 
-function createElements(){
-    const div = document.createElement("div");
-    const idName = String(myLibrary.length - 1);
-    const objKey = `book${idName}`;
-    const lastBook = myLibrary[myLibrary.length - 1][objKey];
-    div.setAttribute("id", objKey);
-    div.setAttribute("class", "book-card");
-    div.innerHTML = `            <div>
+function createElements() {
+  const div = document.createElement("div");
+  const idName = String(myLibrary.length - 1);
+  const objKey = `book${idName}`;
+  const lastBook = myLibrary[myLibrary.length - 1][objKey];
+  div.setAttribute("id", objKey);
+  div.setAttribute("class", "book-card");
+  div.innerHTML = `            <div>
                 <p><strong>Author:</strong> ${lastBook.author}</p>
                 <p><strong>Title:</strong> ${lastBook.title}</p>
                 <p><strong>Pages:</strong> ${lastBook.pages}</p>
@@ -42,56 +42,53 @@ function createElements(){
                 <button class="remove-button">Remove</button>
                 <label>Read<input type="checkbox" class="read-check"></label>
             </div>`;
-    mainContainer.appendChild(div);
-    
-    const removeButton = div.querySelector(".remove-button");
-    removeButton.addEventListener("click", () => {
-        myLibrary.splice(parseInt(idName), 1);
-        mainContainer.removeChild(div);
-    });
-    
-    const readToggle = div.querySelector(".read-check");
-    readToggle.addEventListener("change", () => {
-        lastBook.read = readToggle.checked;
-    })
+  mainContainer.appendChild(div);
 
-};
+  const removeButton = div.querySelector(".remove-button");
+  removeButton.addEventListener("click", () => {
+    myLibrary.splice(parseInt(idName), 1);
+    mainContainer.removeChild(div);
+  });
+
+  const readToggle = div.querySelector(".read-check");
+  readToggle.addEventListener("change", () => {
+    lastBook.read = readToggle.checked;
+  });
+}
 
 addButton.addEventListener("click", () => {
-    dialog.showModal();
+  dialog.showModal();
 });
 
 closeButton.addEventListener("click", () => {
-    const authorInput = document.getElementById("author");
-    const titleInput = document.getElementById("title");
-    const pagesInput = document.getElementById("pages");
+  const authorInput = document.getElementById("author");
+  const titleInput = document.getElementById("title");
+  const pagesInput = document.getElementById("pages");
 
-    authorInput.value = '';
-    titleInput.value = '';
-    pagesInput.value = '';
+  authorInput.value = "";
+  titleInput.value = "";
+  pagesInput.value = "";
 
-    dialog.close();
-})
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const authorInput = document.getElementById("author");
-    const titleInput = document.getElementById("title");
-    const pagesInput = document.getElementById("pages");
-
-    const author = authorInput.value;
-    const title = titleInput.value;
-    const page = pagesInput.value;
-
-    addBookToLibrary(author, title, page);
-    createElements();
-
-    authorInput.value = '';
-    titleInput.value = '';
-    pagesInput.value = '';
-
-    dialog.close();
-
+  dialog.close();
 });
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const authorInput = document.getElementById("author");
+  const titleInput = document.getElementById("title");
+  const pagesInput = document.getElementById("pages");
+
+  const author = authorInput.value;
+  const title = titleInput.value;
+  const page = pagesInput.value;
+
+  addBookToLibrary(author, title, page);
+  createElements();
+
+  authorInput.value = "";
+  titleInput.value = "";
+  pagesInput.value = "";
+
+  dialog.close();
+});
